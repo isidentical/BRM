f4441420e8b1b8d25f5076b1f463341f5376b7cb
isidentical <batuhanosmantaskaya@gmail.com>
2020-01-19 03:32:23 +0300

    index.html, some special cases

--- visualize.py
+++ visualize.py
@@ -1,3 +1,4 @@
+import token
 import tokenize
 from argparse import ArgumentParser
 from pathlib import Path
@@ -7,7 +8,7 @@ import svgwrite
 from brm import TokenTransformer
 
 
-def create_board(transformer, tokens, highlight, filename):
+def create_board(transformer, stream_tokens, highlight, filename):
     drawing = svgwrite.Drawing(filename=filename)
     drawing.add(
         drawing.rect(
@@ -19,18 +20,28 @@ def create_board(transformer, tokens, highlight, filename):
         )
     )
     text = drawing.text("BRM", font_family="monospace")
-    cursor = drawing.tspan(
-        f"Current token: {transformer._get_name(highlight)}"
-    )
-    for token in tokens:
-        source = token.line[token.start[1] : token.end[1]]
-        span = drawing.tspan(
-            source, insert=(50 + token.start[1] * 7, 25 + token.start[0] * 25)
-        )
-        if highlight == token:
+    for stream_token in stream_tokens:
+        x_pos = 50 + stream_token.start[1] * 7
+        y_pos = 25 + stream_token.start[0] * 25
+        source = stream_token.line[
+            stream_token.start[1] : stream_token.end[1]
+        ]  # single line
+        if highlight == stream_token:
+            if stream_token.type == token.NEWLINE:
+                source = "|"
+            elif stream_token.type == token.INDENT:
+                source = "*" * len(stream_token.string)
+
+        span = drawing.tspan(source, insert=(x_pos, y_pos))
+        if highlight == stream_token:
             span.stroke(color="red")
         text.add(span)
 
+    cursor = drawing.tspan(
+        f"Current token: {transformer._get_name(highlight)}", (100, y_pos + 50)
+    )
+    cursor.stroke(color="black")
+    text.add(cursor)
     drawing.add(text)
     drawing.save()
     return drawing.tostring()
@@ -46,13 +57,20 @@ def main():
     with tokenize.open(args.file) as source:
         tokens = transformer.quick_tokenize(source.read())
 
+    if tokens[-1].end[0] > 15:
+        raise ValueError("input file should contain less then 15 lines")
     if not args.output.is_dir():
         raise ValueError("output should be an existing directory")
 
+    sources = []
     for frame, token in enumerate(tokens):
-        create_board(
-            transformer, tokens, token, args.output / f"frame_{frame}.svg"
+        sources.append(
+            create_board(
+                transformer, tokens, token, args.output / f"frame_{frame}.svg"
+            )
         )
+    with open(args.output / "index.html", "w") as index:
+        index.write("\n\n\n".join(sources))
 
 
 if __name__ == "__main__":
